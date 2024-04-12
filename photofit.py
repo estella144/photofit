419ada8fe61bcc5493dcb8474b9e9f45379d143b
Oliver <oliverng2512@icloud.com>
2024-04-12 13:36:33 +0000

    first commit

---
+++ photofit.py
@@ -0,0 +1,23 @@
+import turtle
+window = turtle.Screen()
+
+def draw_eye(color, x, y):
+    pass
+
+def draw_eyes(color):
+    pass
+
+def draw_glasses():
+    pass
+
+def draw_mustache(color):
+    pass
+
+def draw_beard(color):
+    pass
+
+def draw_hair(color, style):
+    pass
+
+def draw_face(skin_color):
+    pass
